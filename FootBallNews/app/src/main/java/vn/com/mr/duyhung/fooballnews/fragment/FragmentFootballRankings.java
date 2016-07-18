2fb213558885779f056c1a4fa1bf09365b7895cd
DuyHung <duyhung94@gmail.com>
2016-07-18 16:33:57 +0700

    [FooballNews 1.01.005] re-test

---
+++ FootBallNews/app/src/main/java/vn/com/mr/duyhung/fooballnews/fragment/FragmentFootballRankings.java
@@ -0,0 +1,13 @@
+package vn.com.mr.duyhung.fooballnews.fragment;
+
+import vn.com.mr.duyhung.fooballnews.activity.BaseFragmentActivity;
+
+/**
+ * Created by _____Mr on 7/18/2016.
+ */
+public class FragmentFootballRankings extends BaseFragmentActivity {
+    @Override
+    protected int getLayoutResourceId() {
+        return 0;
+    }
+}
